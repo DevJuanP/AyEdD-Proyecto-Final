49224ffbe8d2b861dbf542b6394eb80bad7e6bc0
LENOVO <LENOVO@DESKTOP-E4G6K96>
2025-06-04 23:26:58 -0500

    Creacion de class retiro

--- Proyecto TechNexus/src/clases/Retiro.java
+++ Proyecto TechNexus/src/clases/Retiro.java
@@ -1,5 +1,61 @@
 package clases;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Retiro {
 
+	private int numRetiro, numMatricula;
+	private String fecha, hora;
+	private static int contador;
+	
+	static {
+		contador = 200001;
+	}
+
+	public Retiro( int numMatricula) {
+		this.numRetiro = contador++;
+		this.numMatricula = numMatricula;
+		this.fecha = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		this.hora = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+	}
+
+	//get and set
+	
+	public int getNumRetiro() {
+		return numRetiro;
+	}
+
+	public void setNumRetiro(int numRetiro) {
+		this.numRetiro = numRetiro;
+	}
+
+	public int getNumMatricula() {
+		return numMatricula;
+	}
+
+	public void setNumMatricula(int numMatricula) {
+		this.numMatricula = numMatricula;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+	
+
+	
+	
 }
