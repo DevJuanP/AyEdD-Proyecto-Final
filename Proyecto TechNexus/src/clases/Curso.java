2f1eed0a4f2be5de36dd240b3b0df48996b8248e
Jeremy-Cldrn <jeremycldrn.chavez02@gmail.com>
2025-06-11 00:04:14 -0400

    cambios en mantenimiento

--- Proyecto TechNexus/src/clases/Curso.java
+++ Proyecto TechNexus/src/clases/Curso.java
@@ -11,46 +11,48 @@ public class Curso {
 		this.horas = horas;
 		this.asignatura = asignatura;
 	}
-	
+
 	public int getCodCurso() {
 		return codCurso;
 	}
-	
+
 	public void setCodCurso(int codCurso) {
 		this.codCurso = codCurso;
 	}
-	
+
 	public int getCiclo() {
 		return ciclo;
 	}
-	
+
 	public void setCiclo(int ciclo) {
 		this.ciclo = ciclo;
 	}
-	
+
 	public int getCreditos() {
 		return creditos;
 	}
-	
+
 	public void setCreditos(int creditos) {
 		this.creditos = creditos;
 	}
-	
+
 	public int getHoras() {
 		return horas;
 	}
-	
+
 	public void setHoras(int horas) {
 		this.horas = horas;
 	}
-	
+
 	public String getAsignatura() {
 		return asignatura;
 	}
-	
+
 	public void setAsignatura(String asignatura) {
 		this.asignatura = asignatura;
 	}
+	
+
 	
 	
 
--- Proyecto TechNexus/src/gui/DialogMantenimientoCurso.java
+++ Proyecto TechNexus/src/gui/DialogMantenimientoCurso.java
@@ -8,18 +8,26 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.Cursor;
+
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
-import javax.swing.JSeparator;
-import javax.swing.JRadioButton;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.DefaultComboBoxModel;
 
-public class DialogMantenimientoCurso extends JFrame {
+import clases.Curso;
+
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JRadioButton;
+import javax.swing.JSeparator;
+import javax.swing.*;
+
+public class DialogMantenimientoCurso extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -29,30 +37,28 @@ public class DialogMantenimientoCurso extends JFrame {
 	private JLabel lblCreditos;
 	private JLabel lblHoras;
 	private JLabel lblCodigo;
-	private JLabel lblConsultar;
 	private JTextField txtAsignatura;
 	private JTextField txtCreditos;
 	private JTextField txtHoras;
 	private JTextField txtCodigo;
-	private JTextField txtField;
 	private JComboBox<String> cmbCiclo;
-	private JRadioButton rbtnNombresYApellido;
-	private JRadioButton rbtnDni;
-	private JButton btnBuscar;
-	private JButton btnAdicionar;
+	private JButton btnRegistrar;
 	private JButton btnModificar;
 	private JButton btnEliminar;
-	private JButton btnNuevo;
 	private JScrollPane scrollPane;
 	private JTable table;
+	private JLabel lblConsultar;
+	private JTextField txtCodigoCurso;
+	private JButton btnBuscar;
 	private JSeparator separator;
 	private JSeparator separator_1;
-	private JSeparator separator_1_1;
 	private JSeparator separator_2;
-
-	/**
-	 * Launch the application.
-	 */
+	private JSeparator separator_3;
+	private JButton btnNuevo;
+	private JRadioButton rbtnCodigo;
+	private JRadioButton rbtnAsignatura;
+	private int filaSeleccionada = -1;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -65,14 +71,14 @@ public class DialogMantenimientoCurso extends JFrame {
 			}
 		});
 	}
+	
 
-	/**
-	 * Create the frame.
-	 */
+
 	public DialogMantenimientoCurso() {
+		
 		setTitle("Mantenimiento de curso");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 641, 500);
+		setBounds(100, 100, 637, 467);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -91,17 +97,17 @@ public class DialogMantenimientoCurso extends JFrame {
 		contentPane.add(lblMantenimientoCurso);
 		
 		lblAsignatura = new JLabel("Asignatura:");
-		lblAsignatura.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblAsignatura.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblAsignatura.setBounds(23, 43, 132, 20);
 		contentPane.add(lblAsignatura);
 		
 		lblCiclo = new JLabel("Ciclo:");
-		lblCiclo.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblCiclo.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblCiclo.setBounds(23, 73, 46, 20);
 		contentPane.add(lblCiclo);
 		
 		lblCreditos = new JLabel("Cr\u00E9ditos:");
-		lblCreditos.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblCreditos.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblCreditos.setBounds(257, 73, 58, 20);
 		contentPane.add(lblCreditos);
 		
@@ -111,7 +117,7 @@ public class DialogMantenimientoCurso extends JFrame {
 		contentPane.add(lblHoras);
 		
 		lblCodigo = new JLabel("C\u00F3digo:");
-		lblCodigo.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblCodigo.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblCodigo.setBounds(437, 43, 69, 20);
 		contentPane.add(lblCodigo);
 		
@@ -121,124 +127,225 @@ public class DialogMantenimientoCurso extends JFrame {
 		contentPane.add(txtAsignatura);
 		
 		txtCreditos = new JTextField();
-		txtCreditos.setBounds(320, 75, 96, 19);
+		txtCreditos.setBounds(325, 75, 96, 19);
 		contentPane.add(txtCreditos);
 		txtCreditos.setColumns(10);
 		
 		txtHoras = new JTextField();
 		txtHoras.setColumns(10);
-		txtHoras.setBounds(505, 75, 96, 19);
+		txtHoras.setBounds(505, 75, 101, 19);
 		contentPane.add(txtHoras);
 		
 		txtCodigo = new JTextField();
 		txtCodigo.setColumns(10);
-		txtCodigo.setBounds(506, 45, 96, 19);
+		txtCodigo.setBounds(506, 45, 100, 19);
 		contentPane.add(txtCodigo);
 		
 		cmbCiclo = new JComboBox<String>();
 		cmbCiclo.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		cmbCiclo.setModel(new DefaultComboBoxModel<String>(new String[] {"0-Primero", "1-Segundo", "2-Tercero", "3-Cuarto", "4-Quinto", "5-Sexto"}));
+		cmbCiclo.setModel(new DefaultComboBoxModel<String>(new String[] {"0", "1", "2", "3", "4", "5"}));
 		cmbCiclo.setBounds(108, 73, 139, 20);
 		contentPane.add(cmbCiclo);
 		
-		lblConsultar = new JLabel("CONSULTAR:");
-		lblConsultar.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblConsultar.setBounds(53, 116, 102, 20);
-		contentPane.add(lblConsultar);
-		
-		rbtnNombresYApellido = new JRadioButton("Nombres y Apellido");
-		rbtnNombresYApellido.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		rbtnNombresYApellido.setBounds(29, 142, 132, 21);
-		contentPane.add(rbtnNombresYApellido);
-		
-		rbtnDni = new JRadioButton("DNI");
-		rbtnDni.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		rbtnDni.setBounds(190, 142, 46, 21);
-		contentPane.add(rbtnDni);
-		
-		txtField = new JTextField();
-		txtField.setBounds(265, 144, 216, 19);
-		contentPane.add(txtField);
-		txtField.setColumns(10);
-		
-		btnBuscar = new JButton("BUSCAR");
-		btnBuscar.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnBuscar.setBounds(510, 143, 85, 21);
-		contentPane.add(btnBuscar);
-		
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(23, 203, 578, 194);
+		scrollPane.setBounds(23, 202, 583, 141);
 		contentPane.add(scrollPane);
 		
 		table = new JTable();
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-			},
-			new String[] {
-				"C\u00D3DIGO", "ASIGNATURA", "CICLO", "CR\u00C9DITOS", "HORAS"
-			}
-		));
 		scrollPane.setViewportView(table);
 		
-		btnAdicionar = new JButton("REGISTRAR");
-		btnAdicionar.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnAdicionar.setBounds(180, 418, 107, 21);
-		contentPane.add(btnAdicionar);
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("Código");
+		model.addColumn("Asignatura");
+		model.addColumn("Ciclo");
+		model.addColumn("Créditos");
+		model.addColumn("Horas");
+		table.setModel(model);
+
+		btnRegistrar = new JButton("REGISTRAR");
+		btnRegistrar.addActionListener(this);
+		btnRegistrar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnRegistrar.setBounds(185, 368, 107, 21);
+		contentPane.add(btnRegistrar);
 		
 		btnModificar = new JButton("MODIFICAR");
+		btnModificar.addActionListener(this);
 		btnModificar.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnModificar.setBounds(329, 418, 107, 21);
+		btnModificar.setBounds(331, 368, 107, 21);
 		contentPane.add(btnModificar);
 		
 		btnEliminar = new JButton("ELIMINAR");
+		btnEliminar.addActionListener(this);
 		btnEliminar.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnEliminar.setBounds(478, 418, 107, 21);
+		btnEliminar.setBounds(477, 368, 107, 21);
 		contentPane.add(btnEliminar);
 		
+		lblConsultar = new JLabel("CONSULTAR:");
+		lblConsultar.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblConsultar.setBounds(53, 114, 102, 20);
+		contentPane.add(lblConsultar);
+		
+		txtCodigoCurso = new JTextField();
+		txtCodigoCurso.setColumns(10);
+		txtCodigoCurso.setBounds(278, 142, 216, 19);
+		contentPane.add(txtCodigoCurso);
+		
+		btnBuscar = new JButton("BUSCAR");
+		btnBuscar.addActionListener(this);
+		btnBuscar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnBuscar.setBounds(506, 141, 85, 21);
+		contentPane.add(btnBuscar);
+		
 		separator = new JSeparator();
-		separator.setForeground(new Color(128, 128, 128));
-		separator.setBounds(23, 113, 583, 33);
+		separator.setForeground(Color.GRAY);
+		separator.setBounds(23, 111, 583, 33);
 		contentPane.add(separator);
 		
 		separator_1 = new JSeparator(SwingConstants.VERTICAL);
-		separator_1.setForeground(new Color(128, 128, 128));
-		separator_1.setBounds(23, 113, 20, 64);
+		separator_1.setForeground(Color.GRAY);
+		separator_1.setBounds(23, 111, 20, 64);
 		contentPane.add(separator_1);
 		
 		separator_2 = new JSeparator();
-		separator_2.setForeground(new Color(160, 160, 160));
-		separator_2.setBounds(23, 176, 583, 33);
+		separator_2.setForeground(Color.GRAY);
+		separator_2.setBounds(23, 174, 583, 33);
 		contentPane.add(separator_2);
 		
-		separator_1_1 = new JSeparator(SwingConstants.VERTICAL);
-		separator_1_1.setForeground(new Color(128, 128, 128));
-		separator_1_1.setBounds(605, 113, 20, 64);
-		contentPane.add(separator_1_1);
+		separator_3 = new JSeparator(SwingConstants.VERTICAL);
+		separator_3.setForeground(Color.GRAY);
+		separator_3.setBounds(605, 111, 20, 64);
+		contentPane.add(separator_3);
 		
 		btnNuevo = new JButton("NUEVO");
+		btnNuevo.addActionListener(this);
 		btnNuevo.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnNuevo.setBounds(42, 418, 96, 21);
+		btnNuevo.setBounds(39, 368, 107, 21);
 		contentPane.add(btnNuevo);
 		
+		rbtnCodigo = new JRadioButton("C\u00F3digo");
+		rbtnCodigo.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		rbtnCodigo.setBounds(52, 140, 85, 21);
+		contentPane.add(rbtnCodigo);
+		
+		rbtnAsignatura = new JRadioButton("Asignatura");
+		rbtnAsignatura.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		rbtnAsignatura.setBounds(144, 141, 103, 21);
+		contentPane.add(rbtnAsignatura);
+		
+		ButtonGroup grupoBusqueda = new ButtonGroup();
+		grupoBusqueda.add(rbtnCodigo);
+		grupoBusqueda.add(rbtnAsignatura);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnBuscar) {
+			doBtnBuscarActionPerformed(e);
+		}
+		if (e.getSource() == btnNuevo) {
+			doBtnNuevoActionPerformed(e);
+		}
+		if (e.getSource() == btnEliminar) {
+			doBtnEliminarActionPerformed(e);
+		}
+		if (e.getSource() == btnModificar) {
+			doBtnModificarActionPerformed(e);
+		}
+		if (e.getSource() == btnRegistrar) {
+			doBtnRegistrarActionPerformed(e);
+		}
+	}
+	protected void doBtnRegistrarActionPerformed(ActionEvent e) {
+		try {
+			// Leer datos de los campos
+			int codCurso = Integer.parseInt(txtCodigo.getText());
+			String asignatura = txtAsignatura.getText();
+			int ciclo = Integer.parseInt((String) cmbCiclo.getSelectedItem());
+			int creditos = Integer.parseInt(txtCreditos.getText());
+			int horas = Integer.parseInt(txtHoras.getText());
+			//Crear objeto
+			Curso c = new Curso(codCurso, ciclo, creditos, horas, asignatura);
+			// Agregar la tabla
+	        DefaultTableModel model = (DefaultTableModel) table.getModel();
+			model.addRow(new Object[] {
+					c.getCodCurso(),
+					c.getAsignatura(),
+					c.getCiclo(),
+					c.getCreditos(),
+					c.getHoras(),
+			});
+		} catch (Exception e2) {
+			JOptionPane.showMessageDialog(this, "Por favor, ingrese valores numéricos válidos en los campos numéricos.");
+		}
+	}
+	protected void doBtnModificarActionPerformed(ActionEvent e) {
+		if (filaSeleccionada == -1) {
+	        JOptionPane.showMessageDialog(this, "Primero debes buscar un curso para modificar.");
+	        return;
+	    }
+
+	    try {
+	        String asignatura = txtAsignatura.getText();
+	        int ciclo = Integer.parseInt(cmbCiclo.getSelectedItem().toString());
+	        int creditos = Integer.parseInt(txtCreditos.getText());
+	        int horas = Integer.parseInt(txtHoras.getText());
+
+	        DefaultTableModel model = (DefaultTableModel) table.getModel();
+
+	        model.setValueAt(asignatura, filaSeleccionada, 1);
+	        model.setValueAt(ciclo, filaSeleccionada, 2);
+	        model.setValueAt(creditos, filaSeleccionada, 3);
+	        model.setValueAt(horas, filaSeleccionada, 4);
+
+	        JOptionPane.showMessageDialog(this, "Curso modificado exitosamente.");
+
+	        // Reset
+	        filaSeleccionada = -1;
+	        txtCodigo.setEditable(true);
+	        doBtnNuevoActionPerformed(e);
+
+	    } catch (Exception ex) {
+	        JOptionPane.showMessageDialog(this, "Error al modificar. Verifica los campos.");
+	    }
+	}
+	protected void doBtnEliminarActionPerformed(ActionEvent e) {
+	}
+	protected void doBtnNuevoActionPerformed(ActionEvent e) {
+		txtCodigo.setText("");
+		txtAsignatura.setText("");
+		cmbCiclo.setSelectedIndex(0);
+		txtCreditos.setText("");
+		txtHoras.setText("");
+	}
+	protected void doBtnBuscarActionPerformed(ActionEvent e) {
+	    String criterio = txtCodigoCurso.getText().trim();
+	    DefaultTableModel model = (DefaultTableModel) table.getModel();
+	    filaSeleccionada = -1;
+
+	    for (int i = 0; i < model.getRowCount(); i++) {
+	        String valorCelda = "";
+
+	        if (rbtnCodigo.isSelected()) {
+	            valorCelda = model.getValueAt(i, 0).toString(); // código
+	        } else if (rbtnAsignatura.isSelected()) {
+	            valorCelda = model.getValueAt(i, 1).toString(); // asignatura
+	        }
+
+	        if (valorCelda.equalsIgnoreCase(criterio)) {
+	            filaSeleccionada = i;
+
+	            // Llenar campos de texto con los datos encontrados
+	            txtCodigo.setText(model.getValueAt(i, 0).toString());
+	            txtAsignatura.setText(model.getValueAt(i, 1).toString());
+	            cmbCiclo.setSelectedItem(model.getValueAt(i, 2).toString());
+	            txtCreditos.setText(model.getValueAt(i, 3).toString());
+	            txtHoras.setText(model.getValueAt(i, 4).toString());
+
+	            // Deshabilitar campo de código para evitar cambios
+	            txtCodigo.setEditable(false);
+	            return;
+	        }
+	    }
+
+	    JOptionPane.showMessageDialog(this, "No se encontró el curso.");
 	}
 }
