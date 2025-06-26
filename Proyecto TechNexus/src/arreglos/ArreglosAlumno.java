6a7eb7a23f53b9ded592b44c59c0c4661d206bab
LENOVO <LENOVO@DESKTOP-E4G6K96>
2025-06-26 01:55:47 -0500

    creacon de metodos

--- Proyecto TechNexus/src/arreglos/ArreglosAlumno.java
+++ Proyecto TechNexus/src/arreglos/ArreglosAlumno.java
@@ -30,7 +30,7 @@ public class ArreglosAlumno {
 		alumnosList.add(a);
 	}
 	
-	public int tamaño() {
+	public int tamanio() {
 		return alumnosList.size();
 	}
 	
@@ -68,7 +68,7 @@ public class ArreglosAlumno {
 	}
 	
 	public int getNextCodigo() {
-		return tamaño()+202500001;
+		return tamanio()+202500001;
 	}
 	
 	public int cargarAlumnos() {
