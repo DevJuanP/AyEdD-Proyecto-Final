bec4ee32611dd40f4eeffd4686ad96dfc75db19b
OscarEdu2709 <i202501166@cibertec.edu.pe>
2025-07-04 00:16:51 -0500

    consulta

--- Proyecto TechNexus/src/gui/DialogNuevaConsulta.java
+++ Proyecto TechNexus/src/gui/DialogNuevaConsulta.java
@@ -7,11 +7,21 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+
+import clases.*;
+import arreglos.*;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JComboBox;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class DialogNuevaConsulta extends JDialog {
 
@@ -19,10 +29,11 @@ public class DialogNuevaConsulta extends JDialog {
 	private final JPanel contentPanel = new JPanel();
 	private JLabel lblConsultar;
 	private JComboBox<String> cmbBuscar;
-	private JButton btnListar;
+	private JButton btnBuscar;
 	private JButton btnLimpiar;
 	private JScrollPane scrollPane;
 	private JTextArea txtS;
+	private JTextField txtCodigo;
 
 	/**
 	 * Launch the application.
@@ -53,22 +64,76 @@ public class DialogNuevaConsulta extends JDialog {
 		contentPanel.add(lblConsultar);
 		
 		cmbBuscar = new JComboBox<>();
+		cmbBuscar.addItem("ALUMNO");
+		cmbBuscar.addItem("CURSO");
+		cmbBuscar.addItem("MATRICULA");
+		cmbBuscar.addItem("RETIRO");
 		cmbBuscar.setBounds(108, 32, 195, 21);
 		contentPanel.add(cmbBuscar);
 		
-		btnListar = new JButton("LISTAR");
-		btnListar.setBounds(313, 32, 85, 21);
-		contentPanel.add(btnListar);
+		btnBuscar = new JButton("BUSCAR");
+		
+		btnBuscar.setBounds(218, 60, 85, 21);
+		contentPanel.add(btnBuscar);
 		
 		btnLimpiar = new JButton("LIMPIAR");
-		btnLimpiar.setBounds(408, 32, 85, 21);
+		btnLimpiar.setBounds(313, 45, 85, 21);
 		contentPanel.add(btnLimpiar);
 		
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(21, 74, 472, 256);
+		scrollPane.setBounds(21, 109, 472, 221);
 		contentPanel.add(scrollPane);
 		
 		txtS = new JTextArea();
 		scrollPane.setViewportView(txtS);
+		
+		txtCodigo = new JTextField();
+		txtCodigo.setBounds(108, 61, 88, 19);
+		contentPanel.add(txtCodigo);
+		txtCodigo.setColumns(10);
+		
+		JLabel lblCodigo = new JLabel("CODIGO:");
+		lblCodigo.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblCodigo.setBounds(21, 61, 88, 22);
+		contentPanel.add(lblCodigo);
+		
+		ArreglosAlumno RRalum = new ArreglosAlumno();
+		ArreglosMatricula RRmatri = new ArreglosMatricula();
+		ArreglosCursos RRcurs = new ArreglosCursos();	
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				  String categoria = (String) cmbBuscar.getSelectedItem();
+				  String codigoTexto = txtCodigo.getText().trim();
+				  if(codigoTexto.isEmpty()) {
+					  JOptionPane.showMessageDialog(null, "Ingrese un código para buscar.");
+			            return;
+				  }
+				  try {
+					  int codigo= Integer.parseInt(codigoTexto);
+					  String resultado = "";
+					  switch (categoria) {
+					  case "ALUMNO":
+						  Alumno a = RRalum.buscarCodigo(codigo);
+						  if(a != null) {
+							  resultado = "Nombre: " + a.getNombres() +
+	                                    "\nApellidos: " + a.getApellidos() +
+	                                    "\nEdad: " + a.getEdad() +
+	                                    "\nDNI: " + a.getDni();
+							  Matricula m = RRmatri.buscarMatricula(codigo);
+							  if(m != null) {
+								  Curso c = RRcurs.buscarCodigo(codigo);
+								  resultado += "\n\nCurso Matriculado: " + c.getAsignatura() + 
+                                          "\nCiclo: " + c.getCiclo();
+							  }
+							  txtS.setText(resultado);
+
+						  }
+					  }
+							  }catch(Exception x) {
+								  JOptionPane.showMessageDialog(null, "Ingrese un código para buscar.");
+						            return;
+							  }
+			}
+		});
 	}
 }
